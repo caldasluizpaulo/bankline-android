1ee368ce055c89785f8b99f497850efd33630376
Luiz Paulo Caldas <luiz21.lupa@gmail.com>
2022-05-07 16:34:47 -0300

    Adicionando um Adapter e Testando Dados Mockados

--- app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
@@ -3,10 +3,12 @@ package me.dio.bankline.ui.statement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import me.dio.bankline.R
 import me.dio.bankline.databinding.ActivityBankStatementBinding
-import me.dio.bankline.databinding.ActivityWelcomeBinding
 import me.dio.bankline.domain.Correntista
+import me.dio.bankline.domain.Movimentacao
+import me.dio.bankline.domain.TipoMovimentacao
 import java.lang.IllegalArgumentException
 
 class BankStatementActivity : AppCompatActivity() {
@@ -26,6 +28,19 @@ class BankStatementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        Log.d("TESTE", "${accountHolder.id}")
+
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatament()
+    }
+
+    private fun findBankStatament(){
+        val dataSet = ArrayList<Movimentacao>()
+        dataSet.add(Movimentacao(1, "07/05/2022", "Lorem Ipsum", 1000.0, TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1, "07/05/2022", "Lorem Ipsum", 1000.0, TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1, "08/05/2022", "Lorem Ipsum", 1000.0, TipoMovimentacao.DESPESA, 1))
+        dataSet.add(Movimentacao(1, "07/05/2022", "Lorem Ipsum", 1000.0, TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1, "10/05/2022", "Lorem Ipsum", 1000.0, TipoMovimentacao.DESPESA, 1))
+        binding.rvBankStatement.adapter = BankStatementAdapter(dataSet)
     }
 }
